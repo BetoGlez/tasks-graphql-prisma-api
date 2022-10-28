1df09c0319a32990782e46901fe30a0eb8a2b26a
Alberto González <agonzalez@tribalyte.com>
2022-10-28 15:41:23 +0200

    Configure dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core";
+import * as dotenv from "dotenv";
+
+// Preloads config before other imports to ensure env variables are available asap
+dotenv.config();
+
+import ApiConfig from "./api-constants";
 import { AppModule } from "./app.module";
 
-async function bootstrap() {
+const bootstrap = async (): Promise<void> => {
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix(ApiConfig.API_BASE_PATH);
     await app.listen(3000);
-}
+};
+
 bootstrap();
