0ae23ac3958346ffff3bd492f01e5795ced0ff88
Alberto González <agonzalez@tribalyte.com>
2022-11-02 11:10:50 +0100

    Add prisma service config

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ dotenv.config();
 
 import { AppModule } from "./app.module";
 import ApiConfig from "./api-constants";
+import { PrismaService } from "../prisma/prisma.service";
 
 const bootstrap = async (): Promise<void> => {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix(ApiConfig.API_BASE_PATH);
+
+    // Prisma
+    const prismaService = app.get(PrismaService);
+    await prismaService.enableShutdownHooks(app);
+
     await app.listen(3000);
 };
 
